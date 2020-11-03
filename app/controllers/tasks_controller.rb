66be8cf6067ccbc291a0642b240c7d0114186aef
ryoya.nakamura <ryoya19931012@gmail.com>
2020-11-03 02:01:18 +0000

    kadai-tasklist_Ver2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,11 @@
 class TasksController < ApplicationController
+before_action :set_task, only: [:show, :edit, :update, :destroy]
+    
     def index
         @tasks = Task.all
     end
     
     def show
-        #1つだけ取得するので、変数名も単数形
-        #GETメソッド・POSTメソッド・URLのデータはすべてparamsに代入して受け取れる
-        @task = Task.find(params[:id])
     end 
     
     def new
@@ -21,16 +20,14 @@ class TasksController < ApplicationController
             redirect_to @task
         else
             flash.now[:danger] = 'Taskが追加されませんでした'
+            render :new
         end
     end 
     
     def edit
-        @task = Task.find(params[:id])
     end 
     
     def update
-        @task = Task.find(params[:id])
-        
         if @task.update(task_params)
             flash[:success] = 'Taskは正常に更新されました'
             redirect_to @task
@@ -41,7 +38,6 @@ class TasksController < ApplicationController
     end 
     
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
         
         flash[:success] = 'Taskは正常に削除されました'
@@ -49,8 +45,13 @@ class TasksController < ApplicationController
     end 
     
     private
+    
+    def set_task
+        @task = Task.find(params[:id])
+    end
+    
     #Strong Parameter
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content, :status)
     end
 end
